756523e0829135d0872b5c2cf516fdcc729a8bff
AngeloMassaro <angeloemme80@alice.it>
2020-12-29 17:37:21 +0100

    commit

--- src/app/componenti/primo-component/primo-component.component.spec.ts
+++ src/app/componenti/primo-component/primo-component.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PrimoComponentComponent } from './primo-component.component';
 
@@ -6,7 +6,7 @@ describe('PrimoComponentComponent', () => {
   let component: PrimoComponentComponent;
   let fixture: ComponentFixture<PrimoComponentComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PrimoComponentComponent ]
     })
--- src/app/componenti/secondo-component/secondo-component.component.spec.ts
+++ src/app/componenti/secondo-component/secondo-component.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SecondoComponentComponent } from './secondo-component.component';
 
@@ -6,7 +6,7 @@ describe('SecondoComponentComponent', () => {
   let component: SecondoComponentComponent;
   let fixture: ComponentFixture<SecondoComponentComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SecondoComponentComponent ]
     })
